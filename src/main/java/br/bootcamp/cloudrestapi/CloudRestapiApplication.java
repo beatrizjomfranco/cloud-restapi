b2883ed1d5ec888d783487c5e6fcb547ebfc00f6
Beatriz Juliana <beatrizjomfranco@gmail.com>
2022-11-14 23:49:22 -0300

    atualização dos arquivos

---
+++ src/main/java/br/bootcamp/cloudrestapi/CloudRestapiApplication.java
@@ -0,0 +1,13 @@
+package br.bootcamp.cloudrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/bootcamp/cloudrestapi/CloudRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.bootcamp.cloudrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
